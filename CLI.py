213ad11ead5eef5cfabf4f5bb71362352d24f1a0
villi02 <villiarnar1@gmail.com>
2024-03-14 15:46:27 +0100

    made cli a while loop instead of single use

--- CLI.py
+++ CLI.py
@@ -12,54 +12,54 @@ def clear_screen():
         subprocess.run(['clear'], shell=True, check=True)
 
 def main_menu():
-    
-    print("""
-        1. Initialize Database and Insert Initial Data
-        2. Insert data
-        3. Check Chair Availability
-        4. Buy Tickets
-        5. Find Actors by Play by date
-        6. What actors are playing in a given play
-        7. Most popular play
-        8. Find co-actors for a given actor
-        9. Exit
-        """)
+    logged_off = False
+    while not logged_off:
+        print("""
+            1. Initialize Database and Insert Initial Data
+            2. Insert data
+            3. Check Chair Availability
+            4. Buy Tickets
+            5. Find Actors by Play by date
+            6. What actors are playing in a given play
+            7. Most popular play
+            8. Find co-actors for a given actor
+            9. Exit
+            """)
 
-    choice = input("Enter your choice: ")
+        choice = input("Enter your choice: ")
 
 
-    if choice == '1':
-        database.initialize_db()
+        if choice == '1':
+            database.initialize_db()
 
-    elif choice == '2':
-            # Insert data
-        pass
-    elif choice == '3':
-        # Implement check chair
-        pass
-    elif choice == '4':
-        # Buy Tickets
-        pass
-    elif choice == '5':
-        # Find actors by play by date
-        pass
-    elif choice == '6':
-        # List What actors are playing in a given play
-        pass
-    elif choice == '7':
-        # Most popular play
-        # 8. Find co-actors for a given actor for a given actor
-        pass
-    elif choice == '8':
-        # Most popular play
-        # 8. Find co-actors for a given actor for a given actor
-        pass
-    elif choice == '9':
-        print("Exiting...")
-        time.sleep(2)
-        clear_screen()
-    else:
-        print("Invalid choice. Please choose again.")
+        elif choice == '2':
+                # Insert data
+            pass
+        elif choice == '3':
+            # Implement check chair
+            pass
+        elif choice == '4':
+            # Buy Tickets
+            pass
+        elif choice == '5':
+            # Find actors by play by date
+            pass
+        elif choice == '6':
+            # List What actors are playing in a given play
+            pass
+        elif choice == '7':
+            # Most popular play
+            # 8. Find co-actors for a given actor for a given actor
+            pass
+        elif choice == '8':
+            # Most popular play
+            # 8. Find co-actors for a given actor for a given actor
+            pass
+        elif choice == '9':
+            print("Exiting...")
+            logged_off = True
+        else:
+            print("Invalid choice. Please choose again.")
 
 def manage_theater_halls():
     
