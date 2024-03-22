8ac10414484b470271289c5fac0d0f0a7f851f3a
villi02 <villiarnar1@gmail.com>
2024-03-22 01:44:55 +0100

    updated main to proper

--- main.py
+++ main.py
@@ -3,17 +3,10 @@ import database
 import CLI as interface
 import database as datab
 import crud as c
+
 datab.initialize_db()
 
 if __name__ == "__main__":
-    conn = sqlite3.connect('theater.db')
+    conn = sqlite3.connect("theater.db")
     cursor = conn.cursor()
-    
-    print(c.get_names_of_actors_in_various_playes(conn))
-    
-    
-
-    
-   
-  
-   
+    interface.main_menu(conn)
